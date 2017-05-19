bbcf9ed9b8f54206182bf237fcf3a7ba57314b53
pingchang.ke <kpc@vincross.com>
2017-05-19 10:02:38 +0800

    jfButton

---
+++ JDJellyButton/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  JDJellyButton
+//
+//  Created by JamesDouble on 2016/12/9.
+//  Copyright © 2016年 jamesdouble. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    var button:JDJellyButton!
+    @IBOutlet weak var exampleimg: UIImageView!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+
+    
+    let images:[UIImage] = [UIImage(named: "badoo")!,UIImage(named: "behance")!]
+    var imagearr:[[UIImage]] = [[UIImage]]()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        imagearr.append(images)
+        
+        
+        button = JDJellyButton()
+        button.attachtoView(rootView: self.view,mainbutton: UIImage(named:"vk")!)
+        button.delegate = self
+        button.datasource = self
+        button.setJellyType(type: .Cross)
+    }
+
+}
+
+extension ViewController:JellyButtonDelegate
+{
+    func JellyButtonHasBeenTap(touch:UITouch,image:UIImage,groupindex:Int,arrindex:Int)
+    {
+        self.exampleimg.image = image
+        label1.text = "Group Index:\(groupindex)"
+        label2.text = "ArrIndex\(arrindex)"
+    }
+    
+}
+
+extension ViewController:JDJellyButtonDataSource
+{
+    func groupcount()->Int
+    {
+    return 1
+    }
+    func imagesource(forgroup groupindex:Int) -> [UIImage]
+    {
+    return imagearr[groupindex]
+    }
+}
